e0baef3145d3b5c561c2f8dfe39fee10f38bcd49
devprajwalgotnochill <devprajwolgotnochill@gmail.com>
2026-05-13 22:21:27 +0545

    reational operators -> day 3 end

---
+++ week1/day3/reationalOperators.java
@@ -0,0 +1,13 @@
+package week1.day3;
+
+public class reationalOperators {
+       public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a < b);  // true
+        System.out.println(a > b);  // false
+        System.out.println(a <= 10); // true
+        System.out.println(b >= 30); // false
+       }
+}
