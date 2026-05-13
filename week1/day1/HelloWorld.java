6000fa57fc3b78f6e0e16f0901b71f8e880537d0
devprajwalgotnochill <devprajwolgotnochill@gmail.com>
2026-05-13 17:37:41 +0545

    starting day 2

--- week1/day1/HelloWorld.java
+++ week1/day1/HelloWorld.java
@@ -2,7 +2,14 @@ package week1.day1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        // This prints the text to the console
         System.out.println("Hello, World!"); 
+        name();
+    }
+    static void name() {
+        System.out.println("Hello, World!");
     }
 }
+
+
+// static vs dynamic
+//
--- week1/day2/DataTypes.java
+++ week1/day2/DataTypes.java
@@ -4,5 +4,23 @@ public class DataTypes {
     public static void main(String[] args) {
         // This prints the text to the console
         System.out.println("Data types in java Day-2;"); 
+
+        int x = 10;
+
+        System.out.println(x); 
+
+        String name = "prajwal";
+        System.out.println(name.getClass());//doesnt work for premetive ?? 
+
+        System.out.println(name); 
+
+        double y = 1.0000000000001;
+
+        System.out.println(y);
+        
+
+
     }
 }
+
+// primitive vs reference data types
