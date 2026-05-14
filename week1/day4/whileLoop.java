8f7cfd3e07cc6f594379b2c2d654f9daa20a0c7d
devprajwalgotnochill <devprajwolgotnochill@gmail.com>
2026-05-14 15:13:46 +0545

    while loop

---
+++ week1/day4/whileLoop.java
@@ -0,0 +1,11 @@
+package week1.day4;
+
+public class whileLoop {
+    public static void main(String[] args) {
+
+        int x = 1;
+        while (x <= 100) {
+            System.out.println(x++);
+        }
+    }
+}
