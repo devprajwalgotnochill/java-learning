26d8f3694b4617dabd3de11b954f935ddc2cbbd7
devprajwalgotnochill <devprajwolgotnochill@gmail.com>
2026-05-16 22:31:58 +0545

    for loop

---
+++ week1/day4/forLoop.java
@@ -0,0 +1,9 @@
+package week1.day4;
+
+public record forLoop() {
+    public static void main(String[] args) {
+        for (int i = 1; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
